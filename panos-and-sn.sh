f6ff1754fa8337521ff55ec48738a9036abe0569
Giggly Bits <shayne@fennon.com>
2024-01-01 12:14:28 -0700

    silliness

--- panos-and-sn.sh
+++ panos-and-sn.sh
@@ -59,7 +59,7 @@ for i in $(echo -e "$equipment");
 		port=$(echo $i | awk 'BEGIN{FS="_";}{print $3}')
 		key=$(echo $i | awk 'BEGIN{FS="_";}{print $4}')
 
-	echo -en "${info}Attempting to access $inv_name...\033[2K\r"
+	echo -en "${info}Attempting to access $inv_name...\033[0K\r"
 
 	sys_info
 	error_check
