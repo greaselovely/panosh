febe2f37b687fa46f7bccbc3b362f787b52e22ca
Giggly Bits <shayne@fennon.com>
2024-01-01 11:48:21 -0700

    silliness

--- dyn_updates.sh
+++ dyn_updates.sh
@@ -46,10 +46,12 @@ function fetch_config() {
 # Extract data from XML and format it
 function extract_and_format_data() {
     ensure_directory_exists "$1"
-    echo $FUNCNAME && ls -al "$1" && exit
+    echo $FUNCNAME && ls -al "$1"
     tar -xf "$1.tgz" -C "$1"
+    echo $FUNCNAME && ls -al "$1"
     local file="$1/running-config.xml"
     local hostname=$(xmllint --xpath "string(//hostname/text())" "$file")
+    echo $FUNCNAME && ls -al "$1" && exit
     printf "%s\n" "$hostname" >> "$dyndump/$win"
 
     # Extract direct child element names under <update-schedule>
