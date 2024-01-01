5393b6852e3a787ecdd734f9ce7eec78bdfdd246
Giggly Bits <shayne@fennon.com>
2024-01-01 11:43:42 -0700

    silliness

--- dyn_updates.sh
+++ dyn_updates.sh
@@ -45,7 +45,7 @@ function fetch_config() {
 
 # Extract data from XML and format it
 function extract_and_format_data() {
-    mkdir -p "$1"
+    ensure_directory_exists "$1"
     ls $1 & exit
     tar -xf "$1.tgz" -C "$1"
     local file="$1/running-config.xml"
